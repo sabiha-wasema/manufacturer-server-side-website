57d6ef6efca421a5ee47f7f825b3315fb93f865c
sabiha-wasema <sabihawasemaa1244@gmail.com>
2022-05-28 12:26:16 +0600

    varify email admin route set

--- index.js
+++ index.js
@@ -64,13 +64,13 @@ async function run() {
             res.send(users);
         });
 
-        /* app.get('/admin/:email', async (req, res) => {
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
             const user = await userCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
             res.send({ admin: isAdmin })
         });
- */
+
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const filter = { email: email };
